4c0d2f0d9f7b4009ed64f42a2cfc059b919d6ef9
kelly-annlessard <kellyann.lessard@icloud.com>
2026-04-08 21:58:38 -0700

    EDA

--- script/EDA.R
+++ script/EDA.R
@@ -1,10 +1,14 @@
 library(Certara.RsNLME) # data
 library(dplyr)
+library(ggplot2)
 
 # DATA ----
 ## Dataset ----
 df <- pkData
 head(df)
+
+df$Gender <- as.factor(df$Gender)
+df$Subject <- as.factor(df$Subject)
 
 
 ## Summary Statistics ----
@@ -27,7 +31,31 @@ df %>%
 
 
 ## EDA ----
+# concentration by actual time
+ggplot(data = df, aes(y = Conc, x = Act_Time, colour = Subject, group = Subject))+
+  geom_point() +
+  geom_line() +
+  labs(x = "Actual Time (h)", y = "Concentration") +
+  theme_light()
 
+# log-concentration by nominal time
+ggplot(data = df, aes(y = log(Conc), x = Nom_Time, colour = Subject, group = Subject))+
+  geom_point() +
+  geom_line() +
+  theme_light()
 
+# log-concentration by actual time and body weight
+ggplot(data = df, aes(y = log(Conc), x = Act_Time, colour = BodyWeight, group = Subject))+
+  geom_point() +
+  geom_line() +
+  labs(x = "Time (h)", y = "Concentration") +
+  scale_colour_viridis_c() +
+  theme_light()
 
-
+# log-concentration by actual time, age and gender
+ggplot(data = df, aes(y = log(Conc), x = Act_Time, colour = Age, shape = Gender, group = Subject))+
+  geom_point() +
+  geom_line() +
+  labs(x = "Time (h)", y = "Concentration") +
+  scale_colour_viridis_c() +
+  theme_light() 
